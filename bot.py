ecfae1e6d43f1ee9ff73eab1c033abc1fab3bddd
Eduardo Júnio <eduardojvr10@gmail.com>
2018-05-09 20:38:10 -0300

    Update banco e bot

--- bot.py
+++ bot.py
@@ -3,12 +3,17 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot = ChatBot('Test')
+bot = ChatBot('Bot do dudu')
 
-conv = ['oi','olá, bom dia!', 'bom dia','bom dia, como vai?', 'estou bem', 'bot burro', 'voce seu merda!']
+conv = ['oi','olá, bom dia!', 'bom dia','bom dia, como vai?', 'estou bem']
+palavroes = [ 'bot burro', 'voce seu merda!','vsf','vai você, imbecil!','vtnc', 'manda sua mãe!']
+filmes = ['Qual seu filmes favorito?','Sou um bot, não assisto filmes!']
+
 
 bot.set_trainer(ListTrainer)
 bot.train(conv)
+bot.train(palavroes)
+bot.train(filmes)
 
 while True:
     quest = input('Você: ')
