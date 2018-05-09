93eaa180c67552779ed9fcd351388d6d675edcd1
Eduardo Júnio <eduardojvr10@gmail.com>
2018-05-09 20:56:44 -0300

    Treinando bot

--- bot.py
+++ bot.py
@@ -8,12 +8,15 @@ bot = ChatBot('Bot do dudu')
 conv = ['oi','olá, bom dia!', 'bom dia','bom dia, como vai?', 'estou bem']
 palavroes = [ 'bot burro', 'voce seu merda!','vsf','vai você, imbecil!','vtnc', 'manda sua mãe!']
 filmes = ['Qual seu filmes favorito?','Sou um bot, não assisto filmes!']
+emocoes = ['tá feliz?', 'tô felizão', 'magoado', 'tô chorando', 'loser','pegou pesado']
+
 
 
 bot.set_trainer(ListTrainer)
 bot.train(conv)
 bot.train(palavroes)
 bot.train(filmes)
+bot.train(emocoes)
 
 while True:
     quest = input('Você: ')
