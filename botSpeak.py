34dfc82d81b87c4c4b7f312325a8af40e7b4f2e6
Eduardo Júnio <eduardojvr10@gmail.com>
2018-05-10 17:11:11 -0300

    Adicionando bot speak

---
+++ botSpeak.py
@@ -0,0 +1,38 @@
+# _*_ coding: utf-8 _*_
+
+#imports do bot
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+# imports do reconhecimento de voz
+import speech_recognition as sr
+import pyttsx3
+import objc
+
+
+bot = ChatBot('Bot do dudu')
+speak = pyttsx3.init()
+
+def Speak(text):  
+    speak.say(text)
+    speak.runAndWait() 
+
+conv = ['Hi','Hello','How are you', 'I"m fine, thanks']
+
+bot.set_trainer(ListTrainer) #define o método de treinamento
+bot.train(conv) #define as listas de conversas
+
+
+voz = sr.Recognizer()
+
+with sr.Microphone() as s:
+    voz.adjust_for_ambient_noise(s) # se adapta ao ruido
+
+    
+    while True:
+        audio = voz.listen(s) # escuta a fala
+        speech = voz.recognize_google(audio, language='pt') # pega a fala
+        
+        response = bot.get_response(speech)
+        print('Você: ',speech)
+        print('Bot: ', response)
+        Speak(response)
