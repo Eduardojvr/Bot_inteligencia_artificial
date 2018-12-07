0d5ccecf5bc0efacadf934bf06dacc551d8ee479
Eduardojvr <eduardojvr10@gmail.com>
2018-12-07 12:52:39 -0200

    Update bot speak

--- botSpeak.py
+++ botSpeak.py
@@ -7,16 +7,17 @@ from chatterbot import ChatBot
 import speech_recognition as sr
 import pyttsx3
 #import objc
-
+import time
 
 bot = ChatBot('Bot do dudu')
 speak = pyttsx3.init()
 
 def Speak(text):  
-    speak.say(text)
+    speak.say(str(text))
     speak.runAndWait() 
 
-conv = ['Hi','Hello','How are you', 'I"m fine, thanks', 'whats your name?','my name is Bot']
+#conv = ['Hi','Hello','How are you', 'I"m fine, thanks', 'whats your name?','my name is Bot']
+conv = ['Oi','Olá, bom dia!','Como você está?', 'Bem, e você?', 'Qual seu nome?','Meu nome é Astolfo!']
 
 bot.set_trainer(ListTrainer) #define o método de treinamento
 bot.train(conv) #define as listas de conversas
@@ -30,11 +31,13 @@ with sr.Microphone() as s:
     while True:
         try:
             audio = voz.listen(s) # escuta a fala
-            #speech = voz.recognize_google(audio, language='pt') # pega a fala
-            speech = voz.recognize_google(audio) # pega a fala    
+            #speech = voz.recognize_sphinx(audio) # pega a fala
+            speech = voz.recognize_google(audio, language='pt') # pega a fala    
+            #speech = input('Digite: ')
+            #speech = str(speech)
             response = bot.get_response(speech)
             print('Você: ',speech)
             print('Bot: ', response)
             Speak(response)
         except:
-            pass
+            print("Erro!")
